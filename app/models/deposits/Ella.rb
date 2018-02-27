f8f6b39018c77097a86432df615cf194be35ff8b
osoese <osoese@ellaism.com>
2018-02-27 01:47:59 -0500

    testing add ella

---
+++ app/models/deposits/Ella.rb
@@ -0,0 +1,15 @@
+module Deposits
+
+  class Ella < ::Deposit
+
+    include ::AasmAbsolutely
+
+    include ::Deposits::Coinable
+
+
+
+    validates_uniqueness_of :txout, scope: :txid
+
+  end
+
+end
