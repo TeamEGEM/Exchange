ea69c0eb32958cdf14282fb61e2c2c06fd374ef3
osoese <osoese@ellaism.com>
2018-02-27 02:18:09 -0500

    adding ella

--- app/models/worker/deposit_coin.rb
+++ app/models/worker/deposit_coin.rb
@@ -58,6 +58,67 @@ module Worker
       Rails.logger.error "txid: #{txid}, txout: #{txout}, detail: #{raw.inspect}"
       Rails.logger.error $!.backtrace.join("\n")
     end
+    
+    module Worker
+  class DepositCoin
+
+    def process(payload, metadata, delivery_info)
+      payload.symbolize_keys!
+
+      sleep 0.5 # nothing result without sleep by query gettransaction api
+
+      channel_key = payload[:channel_key]
+      txid = payload[:txid]
+
+      channel = DepositChannel.find_by_key(channel_key)
+      if channel.currency_obj.code == 'eth'
+        raw  = get_raw_eth txid
+        raw.symbolize_keys!
+        deposit_eth!(channel, txid, 1, raw)
+      else
+        raw  = get_raw channel, txid
+        raw[:details].each_with_index do |detail, i|
+          detail.symbolize_keys!
+          deposit!(channel, txid, i, raw, detail)
+        end
+      end
+    end
+    
+    def deposit_ella!(channel, txid, txout, raw)
+      ActiveRecord::Base.transaction do
+        unless PaymentAddress.where(currency: channel.currency_obj.id, address: raw[:to]).first
+          Rails.logger.info "Deposit address not found, skip. txid: #{txid}, txout: #{txout}, address: #{raw[:to]}, amount: #{raw[:value].to_i(16) / 1e18}"
+          return
+        end
+        return if PaymentTransaction::Normal.where(txid: txid, txout: txout).first
+        confirmations = CoinRPC["ella"].eth_blockNumber.to_i(16) - raw[:blockNumber].to_i(16)
+        tx = PaymentTransaction::Normal.create! \
+        txid: txid,
+        txout: txout,
+        address: raw[:to],
+        amount: (raw[:value].to_i(16) / 1e18).to_d,
+        confirmations: confirmations,
+        receive_at: Time.now.to_datetime,
+        currency: channel.currency
+
+        deposit = channel.kls.create! \
+        payment_transaction_id: tx.id,
+        txid: tx.txid,
+        txout: tx.txout,
+        amount: tx.amount,
+        member: tx.member,
+        account: tx.account,
+        currency: tx.currency,
+        confirmations: tx.confirmations
+
+        deposit.submit!
+        deposit.accept! 
+      end
+    rescue
+      Rails.logger.error "Failed to deposit: #{$!}"
+      Rails.logger.error "txid: #{txid}, txout: #{txout}, detail: #{raw.inspect}"
+      Rails.logger.error $!.backtrace.join("\n")
+    end
 
     def deposit!(channel, txid, txout, raw, detail)
       return if detail[:account] != "payment" || detail[:category] != "receive"
