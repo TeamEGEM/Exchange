6e5a50d78ee6bda93d170cb596b67f3b3e328f7f
jose <jose@kinertia.com>
2018-02-26 20:48:10 -0500

    editing auth verified config

--- app/controllers/private/funds_controller.rb
+++ app/controllers/private/funds_controller.rb
@@ -3,7 +3,7 @@ module Private
     layout 'funds'
 
     before_action :auth_activated!
-    #before_action :auth_verified!
+    #before_action :auth_verified! removed auth verified to access funds controller for non KYC customers (jal3inc)
     before_action :two_factor_activated!
 
     def index
